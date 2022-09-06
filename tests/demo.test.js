7a653ee449f0c1017ccde8e0bff93382c2a3ec77
Oscar <oscarantonio.soto1998@gmail.com>
2022-09-06 11:39:43 -0400

    Fix: learn about expect and toBe

--- tests/demo.test.js
+++ tests/demo.test.js
@@ -1,5 +1,12 @@
-test('Esta prueba no debe de fallar', () => {
-  if(1 === 0) {
-    throw new Error('No puede dividir entre 0')
-  }
-})
+describe("Prueba en <DemoComponent/ >", () => {
+  test("Esta prueba no debe de fallar", () => {
+    // 1. Inicializacion
+    const message1 = "Hola Mundo";
+
+    // 2. Estimulo
+    const message2 = message1.trim();
+
+    // 3. Observar el comportamiento... esperado
+    expect(message1).toBe(message2);
+  });
+});
