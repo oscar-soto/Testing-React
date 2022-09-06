d2efca66e38b16c21ea6ed80c01b94cd39af4627
Oscar <oscarantonio.soto1998@gmail.com>
2022-09-06 17:42:23 -0400

    Fix: test with promise

---
+++ tests/base-pruebas/09-promesas.test.js
@@ -0,0 +1,30 @@
+import { getHeroeByIdAsync } from "../../src/base-pruebas/09-promesas";
+
+describe('Pruebas en 09-promesas', () => {
+  test('getHeroByIdAsync Debe retornar un heroe', (done) => {
+    const id = 1;
+    getHeroeByIdAsync(id)
+      .then(hero => {
+        expect(hero).toEqual({
+          id: 1,
+          name: 'Batman',
+          owner: 'DC',
+        }) 
+        done()
+      })
+  })
+
+  test('getHeroByIdAsync Debe obtener un error si heroe no existe', (done) => {
+    const id = 100;
+    getHeroeByIdAsync(id)
+      .then(hero => {
+        expect( hero ).toBeFalsy()
+        done()
+      })
+      .catch(error => {
+        expect( error ).toBe(`No se pudo encontrar el héroe ${id}`)
+        done()
+      })
+  })
+
+})
