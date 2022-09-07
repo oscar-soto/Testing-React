aa44cbc2c750f6ec06b160067c5f916c095e1acf
Oscar <oscarantonio.soto1998@gmail.com>
2022-09-07 12:36:59 -0400

    Fix: add teact testing library

---
+++ src/FirstApp.jsx
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types";
+
+export const FirstApp = ({ title, subTitle, name }) => {
+  return (
+    <>
+      <h1>{title}</h1>
+      <p>{subTitle}</p>
+      <p>{name}</p>
+    </>
+  );
+};
+
+FirstApp.propTypes = {
+  title: PropTypes.string.isRequired,
+  subTitle: PropTypes.number,
+};
+
+FirstApp.defaultProps = {
+  // title: "No hay titulo",
+  subTitle: 123,
+  name: 'Oscar Espinoza'
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { CounterApp } from "./CounterApp";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { CounterApp } from './CounterApp';
+import { FirstApp } from './FirstApp';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CounterApp value={4} />
+    {/* <CounterApp value={4} /> */}
+    <FirstApp />
   </React.StrictMode>
 );
